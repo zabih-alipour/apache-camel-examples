e6519aa8e18d350c8f6424b7c64ee3b70f80b025
Zabih Alipour <za.alipour.java@gmail.com>
2019-07-06 13:49:54 +0430

    Initialize commit

---
+++ file-copier/src/main/java/com/alipour/learning/filecopier/FileCopierApplication.java
@@ -0,0 +1,13 @@
+package com.alipour.learning.filecopier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCopierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCopierApplication.class, args);
+	}
+
+}
